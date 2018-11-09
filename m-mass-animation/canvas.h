125220363d250cfd5eddaf1daa5851b9727dbea0
Manfred Morgner <manfred.morgner@gmail.com>
2018-11-09 01:31:17 +0100

    massive animation

---
+++ m-mass-animation/canvas.h
@@ -0,0 +1,216 @@
+#include <gtkmm.h>
+#include <gtkmm/drawingarea.h>
+
+#include <vector>
+
+
+struct SPoint
+    {
+    SPoint() = default;
+    SPoint(SPoint const & src) = default;
+    template<typename T>
+        SPoint(T const & x, T const & y) : x(x), y(y) {}
+    template<typename T>
+        SPoint(T const & t) : x(t.x), y(t.y) {}
+    double x{0}, y{0};
+    template<typename P>
+        SPoint operator = (P const & p)
+            {
+            x = p.x;
+            y = p.y;
+            return *this;
+            }
+    template<typename P>
+        SPoint operator -= (P const & p)
+            {
+            x -= p.x;
+            y -= p.y;
+            return *this;
+            }
+    template<typename P>
+        SPoint operator += (P const & p)
+            {
+            x += p.x;
+            y += p.y;
+            return *this;
+            }
+    };
+
+inline bool operator == (SPoint const & p1, SPoint const & p2)
+    {
+    return (p1.x==p2.x) && (p1.y==p2.y);
+    }
+
+inline bool operator != (SPoint const & p1, SPoint const & p2)
+    {
+    return !(p1==p2);
+    }
+
+inline SPoint operator - (SPoint const & p1, SPoint const & p2)
+    {
+    return {p1.x-p2.x, p1.y-p2.y};
+    }
+
+inline SPoint operator + (SPoint const & p1, SPoint const & p2)
+    {
+    return {p2.x+p1.x, p2.y+p1.y};
+    }
+
+inline SPoint operator / (SPoint const & p, double const & d)
+    {
+    return {p.x/d, p.y/d};
+    }
+
+inline SPoint operator * (SPoint const & p, double const & d)
+    {
+    return {p.x*d, p.y*d};
+    }
+
+
+struct SFleck
+    {
+    double x{0}, y{0}, r{0};
+    template<typename P>
+        SFleck operator -= (P const & p)
+            {
+            x -= p.x;
+            y -= p.y;
+            return *this;
+            }
+    template<typename P>
+        SFleck operator += (P const & p)
+            {
+            x += p.x;
+            y += p.y;
+            return *this;
+            }
+    template<typename P>
+        SFleck operator = (P const & p)
+            {
+            x = p.x;
+            y = p.y;
+            return *this;
+            }
+   };
+
+struct SColor
+    {
+    double r{0},g{0},b{0};
+    };
+
+using VPoints  = std::vector<SPoint>;
+using VFlecken = std::vector<SFleck>;
+
+// the first argument can be anything having the members x and y
+// a Gtk event, a SFleck or whatever
+template<typename P, typename T>
+    double Distance( P const & a, T const & b )
+        {
+        return sqrt( pow((a.x-b.x),2) + pow((a.y-b.y),2) );
+        }
+
+
+class CCanvas : public Gtk::DrawingArea
+    {
+    public:
+        CCanvas()
+            {
+            add_events(Gdk::BUTTON_PRESS_MASK | Gdk::SCROLL_MASK | Gdk::SMOOTH_SCROLL_MASK);
+            add_events(Gdk::BUTTON_PRESS_MASK | Gdk::BUTTON_RELEASE_MASK);
+            add_events(Gdk::BUTTON1_MOTION_MASK | Gdk::BUTTON_PRESS_MASK | Gdk::POINTER_MOTION_MASK);
+            add_events(Gdk::KEY_PRESS_MASK | Gdk::KEY_RELEASE_MASK);
+
+            m_fSlot[0]         = sigc::bind(sigc::mem_fun(*this, &CCanvas::AnimateBi), 0);
+            m_fSlot[1]         = sigc::bind(sigc::mem_fun(*this, &CCanvas::AnimateRot), 0);
+            m_afConnections[0] = Glib::signal_timeout().connect(m_fSlot[0], 5);
+            m_afConnections[1] = Glib::signal_timeout().connect(m_fSlot[1], 5);
+            for (double x{-570}; x < 570; x+=40) 
+               for (double y{-570}; y < 570; y+=40) 
+                    m_vFlecken.emplace_back( SFleck{x,y,20} );
+            }
+
+        virtual ~CCanvas()
+            {
+            m_afConnections[0].disconnect();
+            m_afConnections[1].disconnect();
+            };
+
+    protected:
+        // Override default signal handler:
+        bool on_draw(const Cairo::RefPtr<Cairo::Context>& cr) override;
+        bool on_scroll_event(GdkEventScroll *event) override;
+        bool on_button_press_event(GdkEventButton * event) override;
+        bool on_motion_notify_event(GdkEventMotion *event) override;
+        bool on_button_release_event(GdkEventButton* release_event) override;
+
+        bool Collision(SPoint const & tPoint);
+        
+        SPoint   m_tCtxSize     { .0,.0 };
+        double   m_dScale       { 1.0 };
+        
+        bool     m_bShiftInit   { true };
+        SPoint   m_tShift       { .0,.0 };
+        SPoint   m_tEventPress  { .0,.0 };
+        SPoint   m_tEventRelease{ .0,.0 };
+        SPoint   m_tShiftStart  { .0,.0 };
+
+       
+        SPoint   m_tMousePos;
+        SColor   m_tMouseColor{ .5,.5,.5 };
+        VPoints  m_vMouseTrail;
+        VFlecken m_vFlecken { };
+
+        struct SCollision
+            {
+            SPoint tWhere  { .0,.0 };
+            SPoint tOffset { .0,.0 };
+            enum class EWhat
+                {
+                none,       // there was no collision
+                Fleck,      // move a Fleck
+                Line,       // move a Line
+                }eWhat {EWhat::none};
+            int nIndex {0};	// O: L1, L2, L3
+            int nSubIx {0};	// L: P1, PM, P2
+            } m_tCollision;
+
+        // animation
+        bool             m_bAnimate{true};
+        bool             m_bAnimateLeftBi{true};
+        bool             m_bAnimateLeftRot{true};
+
+        // animation clock
+        double           m_dAnimatorBi{0};  // $m_tAnimator animation parameter
+        double           m_dAnimatorRot{0}; // $m_tAnimator animation parameter
+        double           m_dAnimStep{0};    // intermediate animation parameter
+        sigc::slot<bool> m_fSlot[2];
+        sigc::connection m_afConnections[2];
+
+        double           m_dAnimate   {0.0025}; // animation steps width
+        double const     m_dAnimateMax{0.0250}; // maximal animation step width
+        double const     m_dAnimateMin{0.0025}; // minimal animation step width
+
+        bool AnimateRot(int c) // rotation
+            {
+            if (!m_bAnimate) return true;
+            if (m_bAnimateLeftRot)
+                m_dAnimatorRot = (m_dAnimatorRot <=  m_dAnimate) ? 1 : m_dAnimatorRot-m_dAnimate;
+                else
+                m_dAnimatorRot = (m_dAnimatorRot >=1-m_dAnimate) ? 0 : m_dAnimatorRot+m_dAnimate;
+            queue_draw();
+            return true;
+            }
+
+        bool AnimateBi(int c) // bidirectional
+            {
+            if (!m_bAnimate) return true;
+            if (m_bAnimateLeftBi)
+                m_dAnimatorBi -= m_dAnimate;
+                else
+                m_dAnimatorBi += m_dAnimate;
+            if (m_dAnimatorBi <= 0) m_bAnimateLeftBi = false;
+            if (m_dAnimatorBi >= 1) m_bAnimateLeftBi = true;
+            queue_draw();
+            return true;
+            }
+    }; // CCanvas
